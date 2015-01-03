a7538ba3dc25daf275b4c8eeeb83ee472734a1dd
Alex Nichol <unixpickle@gmail.com>
2015-01-03 14:00:30 -0500

    use a "common words" wordlist to sort examples

--- diphones/main.go
+++ diphones/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/unixpickle/gospeech"
+	"io/ioutil"
 	"os"
 	"sort"
 	"strings"
@@ -17,15 +18,29 @@ func main() {
 }
 
 func ErrMain() error {
-	if len(os.Args) != 2 {
-		return errors.New("Usage: diphones <dictionary.txt>")
+	if len(os.Args) != 3 {
+		return errors.New("Usage: diphones <dictionary.txt> <common.txt>")
 	}
+	
+	// Read input files
+	fmt.Println("Reading input files...")
 	dict, err := gospeech.LoadDictionary(os.Args[1])
 	if err != nil {
 		return err
 	}
+	common, err := ReadLines(os.Args[2])
+	if err != nil {
+		return err
+	}
+	
+	// Sort the dictionary
+	fmt.Println("Sorting dictionary...")
+	dictKeys := SortDictionary(dict, common)
+	
+	// Generate diphones
+	fmt.Println("Generating diphones...")
 	diphones := map[string][]string{}
-	for word, raw := range dict {
+	for _, word := range dictKeys {
 		phones := dict.Get(word)
 		if phones == nil {
 			continue
@@ -38,10 +53,16 @@ func ErrMain() error {
 			} else if !ok {
 				diphones[diphone] = []string{}
 			}
-			example := strings.ToLower(word) + " (" + raw + ")"
+			example := strings.ToLower(word) + " (" + dict[word] + ")"
 			diphones[diphone] = append(diphones[diphone], example)
 		}
 	}
+	
+	PrintDiphones(diphones)
+	return nil
+}
+
+func PrintDiphones(diphones map[string][]string) {
 	keys := make([]string, 0, len(diphones))
 	for key, _ := range diphones {
 		keys = append(keys, key)
@@ -51,5 +72,29 @@ func ErrMain() error {
 		fmt.Println(diphone, "::", strings.Join(diphones[diphone], ", "))
 		fmt.Println("")
 	}
-	return nil
+}
+
+func ReadLines(path string) ([]string, error) {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	list := strings.Split(string(contents), "\n")
+	sort.Strings(list)
+	return list, nil
+}
+
+func SortDictionary(dict gospeech.Dictionary, common []string) []string {
+	inCommon := make([]string, 0)
+	uncommon := make([]string, 0)
+	for word, _ := range dict {
+		w := strings.ToLower(word)
+		idx := sort.SearchStrings(common, w)
+		if idx == len(common) || common[idx] != w {
+			uncommon = append(uncommon, word)
+		} else {
+			inCommon = append(inCommon, word)
+		}
+	}
+	return append(inCommon, uncommon...)
 }
