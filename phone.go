c50d78b0cdecdeb9d77a2075ef4dd6bd0385f3ef
Alex Nichol <unixpickle@gmail.com>
2016-02-04 20:20:52 -0500

    create and use PhoneType type

--- phone.go
+++ phone.go
@@ -6,8 +6,8 @@ import (
 	"github.com/unixpickle/wav"
 )
 
-// transitionFraction is the fraction of a phone's time that is spent
-// transitioning from the previous phone.
+// transitionFraction is the fraction of a stop or fricative's
+// time that is spent transitioning between phones.
 const transitionFraction = 0.5
 
 // consonantPull is the amount of influence consonants should have on
@@ -16,17 +16,30 @@ const consonantPull = 0.2
 
 const maxFormantAmplitude = 0.3
 
+type PhoneType int
+
+const (
+	Stop PhoneType = iota
+	Fricative
+	Nasal
+	Liquid
+	Glide
+	Vowel
+)
+
 type Phone struct {
+	Type     PhoneType
+	Unvoiced bool
+
 	// Duration is the length of the phone in relative units.
 	// The average Duration for a given voice should be about 1.
 	Duration float64
 
+	// For vowels, these fields determine the phone's sound.
+	// For consonants, these determine how vowel-consonant transitions
+	// should take place.
 	Formants       [3]float64
 	FormantVolumes [3]float64
-
-	Consonant bool
-	Voiced    bool
-	Nasal     bool
 
 	// These fields describe the noise produced by a fricative consonant.
 	NoiseFrequency float64
