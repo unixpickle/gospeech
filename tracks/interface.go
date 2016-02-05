2be8c3e8d4c7924439fb114bb5d9de5a3d6ab93e
Alex Nichol <unixpickle@gmail.com>
2016-02-05 18:42:13 -0500

    made TrackSet implement Track
    This makes it possible to encode and nest TrackSets. The nesting functionality is especially nice, because I can have, for instance, a TrackSet called "Formants" inside a larger TrackSet that represents the entire vocal system.

--- tracks/interface.go
+++ tracks/interface.go
@@ -30,9 +30,10 @@ type Track interface {
 	AdjustVolume(newVolume float64, transitionTime time.Duration)
 }
 
+// A TrackID is a string used to identify tracks in a TrackSet.
 type TrackID string
 
-// A TrackSet facilitates bulk manipulation of Tracks.
+// A TrackSet is a track which manages other Tracks in bulk.
 type TrackSet map[TrackID]Track
 
 // ExcludeTracks returns a TrackSet that does not contain tracks
@@ -51,6 +52,42 @@ OuterLoop:
 	return res
 }
 
+// Duration returns the duration of the longest track in the set.
+func (t TrackSet) Duration() (maxDur time.Duration) {
+	for _, track := range t {
+		if d := track.Duration(); d > maxDur {
+			maxDur = d
+		}
+	}
+	return
+}
+
+// Encode generates samples by encoding every track in the set and
+// summing up the signals.
+func (t TrackSet) Encode(sampleRate int) (res []wav.Sample) {
+	sampleCount := 0
+	encodedTracks := make([][]wav.Sample, 0, len(t))
+	for _, track := range t {
+		encodedTrack := track.Encode(sampleRate)
+		encodedTracks = append(encodedTracks, encodedTrack)
+		if len(encodedTrack) > sampleCount {
+			sampleCount = len(encodedTrack)
+		}
+	}
+
+	res = make([]wav.Sample, sampleCount)
+	for i := range res {
+		for _, enc := range encodedTracks {
+			if i >= len(enc) {
+				continue
+			}
+			res[i] += enc[i]
+		}
+	}
+
+	return
+}
+
 // Continue elongates all of the set's tracks by a given duration.
 func (t TrackSet) Continue(duration time.Duration) {
 	for _, track := range t {
@@ -58,8 +95,17 @@ func (t TrackSet) Continue(duration time.Duration) {
 	}
 }
 
+// Volume returns the average volume of the tracks in this set.
+func (t TrackSet) Volume() float64 {
+	var sum float64
+	for _, track := range t {
+		sum += track.Volume()
+	}
+	return sum / float64(len(t))
+}
+
 // AdjustVolume elongates all of the tracks while simultaneously
-// adjusting their volumes.
+// adjusting their volumes to the given value.
 func (t TrackSet) AdjustVolume(newVolume float64, duration time.Duration) {
 	for _, track := range t {
 		track.AdjustVolume(newVolume, duration)
