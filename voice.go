724856d0375fc8335d7f4354a325c4bdd084fa26
Alex Nichol <unixpickle@gmail.com>
2016-02-06 15:17:11 -0500

    use new Phone system for a Voice

--- voice.go
+++ voice.go
@@ -7,272 +7,145 @@ import (
 )
 
 type Voice struct {
-	Phones map[string]*Phone
+	Phones map[string]Phone
 }
 
 func (v Voice) Synthesize(ipaString string, phoneRate float64) wav.Sound {
-	s := wav.NewPCM8Sound(1, 44100)
-	var lastPhone *Phone
+	vocalSystem := NewVocalSystem()
+	var lastPhone Phone
 	for _, ph := range []rune(ipaString) {
 		if ph == ' ' {
-			wav.AppendSilence(s, time.Duration(float64(time.Second)/phoneRate))
+			vocalSystem.AdjustVolume(0, time.Millisecond*50)
+			vocalSystem.Continue(time.Millisecond * 300)
 			lastPhone = nil
 			continue
 		}
 		phone := v.Phones[string(ph)]
 		if phone != nil {
-			phone.Synthesize(lastPhone, s, phoneRate)
+			phone.EncodeBeginning(vocalSystem, lastPhone)
 			lastPhone = phone
 		}
 	}
+	s := wav.NewPCM8Sound(1, 44100)
+	s.SetSamples(vocalSystem.Encode(44100))
 	return s
 }
 
-var DefaultVoice = Voice{Phones: map[string]*Phone{
-	"i": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{280, 2250, 2890},
-		FormantVolumes: [3]float64{1, 1, 1},
+var DefaultVoice = Voice{
+	Phones: map[string]Phone{
+		"i": Vowel{
+			Formants: NewFormantState(280, 0.3, 2250, 0.3, 2890, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"I": Vowel{
+			Formants: NewFormantState(400, 0.3, 1920, 0.3, 2560, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"e": Vowel{
+			Formants: NewFormantState(400, 0.3, 2200, 0.3, 2890, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"ə": Vowel{
+			Formants: NewFormantState(500, 0.3, 1500, 0.3, 2490, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"ɛ": Vowel{
+			Formants: NewFormantState(550, 0.3, 1770, 0.3, 2490, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"ʌ": Vowel{
+			Formants: NewFormantState(550, 0.3, 1000, 0.3, 2490, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"æ": Vowel{
+			Formants: NewFormantState(690, 0.3, 1660, 0.3, 2490, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"u": Vowel{
+			Formants: NewFormantState(310, 0.3, 870, 0.3, 2250, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"ʊ": Vowel{
+			Formants: NewFormantState(450, 0.3, 1030, 0.3, 2380, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"o": Vowel{
+			Formants: NewFormantState(450, 0.3, 700, 0.3, 2380, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"ɔ": Vowel{
+			Formants: NewFormantState(590, 0.3, 880, 0.3, 2540, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"a": Vowel{
+			Formants: NewFormantState(710, 0.3, 1100, 0.3, 2540, 0.3),
+			Duration: time.Millisecond * 200,
+		},
+		"p": BilabialPlosive{Voiced: false},
+		"b": BilabialPlosive{Voiced: true},
+		"t": AlveolarPlosive{Voiced: false},
+		"d": AlveolarPlosive{Voiced: true},
+		"k": VelarPlosive{Voiced: false},
+		"g": VelarPlosive{Voiced: true},
+		"m": Nasal{
+			Type:     "m",
+			Formants: NewFormantState(250, 0.3, 2500, 0.1, 3250, 0.1),
+		},
+		"n": Nasal{
+			Type:     "n",
+			Formants: NewFormantState(250, 0.3, 2500, 0.1, 3250, 0.1),
+		},
+		"ŋ": Nasal{
+			Type:     "ŋ",
+			Formants: NewFormantState(250, 0.3, 2500, 0.1, 3250, 0.1),
+		},
+		"θ": Fricative{
+			Type:   "TH",
+			Voiced: false,
+		},
+		"ð": Fricative{
+			Type:   "TH",
+			Voiced: true,
+		},
+		"f": Fricative{
+			Type:   "F",
+			Voiced: false,
+		},
+		"v": Fricative{
+			Type:   "F",
+			Voiced: true,
+		},
+		"s": Fricative{
+			Type:   "S",
+			Voiced: false,
+		},
+		"z": Fricative{
+			Type:   "S",
+			Voiced: true,
+		},
+		"ʃ": Fricative{
+			Type:   "SH",
+			Voiced: false,
+		},
+		"ʒ": Fricative{
+			Type:   "SH",
+			Voiced: true,
+		},
+		"h": Fricative{
+			Type:   "H",
+			Voiced: false,
+		},
+		"l": LateralLiquid{},
+		"ɹ": RetroflexLiquid{Formants: NewFormantState(500, 0.3, 1500, 0.3, 1900, 0.3)},
+		"j": Vowel{
+			Formants: NewFormantState(280, 0.3, 2250, 0.3, 2890, 0.3),
+			Duration: time.Millisecond * 60,
+		},
+		"w": Vowel{
+			Formants: NewFormantState(310, 0.3, 870, 0.3, 2250, 0.3),
+			Duration: time.Millisecond * 60,
+		},
+		"ʔ": GlottalStop{},
+		"ɾ": AlveolarPlosive{Voiced: false},
 	},
-	"I": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{400, 1920, 2560},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"e": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{400, 2200, 2890},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ə": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{500, 1500, 2490},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ɛ": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{550, 1770, 2490},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ʌ": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{550, 1000, 2490},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"æ": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{690, 1660, 2490},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"u": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{310, 870, 2250},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ʊ": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{450, 1030, 2380},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"o": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{450, 700, 2380},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ɔ": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{590, 880, 2540},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"a": &Phone{
-		Type:           Vowel,
-		Duration:       1,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"p": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{310, 870, 2250},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-	},
-	"b": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{310, 870, 2250},
-		FormantVolumes: [3]float64{0, 0, 0},
-	},
-	"t": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{400, 1920, 2560},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-	},
-	"d": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{400, 1920, 2560},
-		FormantVolumes: [3]float64{0, 0, 0},
-	},
-	"k": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-	},
-	"g": &Phone{
-		Type:           Stop,
-		Duration:       0.5,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{0, 0, 0},
-	},
-	"θ": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{500, 1500, 2490},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-		NoiseFrequency: 1000,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"ð": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{500, 1500, 2490},
-		FormantVolumes: [3]float64{0, 0, 0},
-		NoiseFrequency: 1000,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"f": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{450, 1030, 2380},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-		NoiseFrequency: 1000,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"v": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{450, 1030, 2380},
-		FormantVolumes: [3]float64{0, 0, 0},
-		NoiseFrequency: 1000,
-		NoiseSpread:    200,
-		NoiseVolume:    0.3,
-	},
-	"s": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{400, 1920, 2560},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-		NoiseFrequency: 5000,
-		NoiseSpread:    1000,
-		NoiseVolume:    1,
-	},
-	"z": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{400, 1920, 2560},
-		FormantVolumes: [3]float64{0, 0, 0},
-		NoiseFrequency: 2000,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"ʃ": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{280, 2250, 2890},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-		NoiseFrequency: 1300,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"ʒ": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{280, 2250, 2890},
-		FormantVolumes: [3]float64{0, 0, 0},
-		NoiseFrequency: 1300,
-		NoiseSpread:    200,
-		NoiseVolume:    1,
-	},
-	"h": &Phone{
-		Type:           Fricative,
-		Duration:       0.7,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-		NoiseFrequency: 600,
-		NoiseSpread:    600,
-		NoiseVolume:    1,
-	},
-	"m": &Phone{
-		Type:           Nasal,
-		Duration:       0.7,
-		Formants:       [3]float64{450, 1030, 2380},
-		FormantVolumes: [3]float64{0, 0, 0},
-	},
-	"n": &Phone{
-		Type:           Nasal,
-		Duration:       0.7,
-		Formants:       [3]float64{500, 1500, 2490},
-		FormantVolumes: [3]float64{0, 0, 0},
-	},
-	"ŋ": &Phone{
-		Type:           Nasal,
-		Duration:       0.7,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{0.7, 0.7, 0.7},
-	},
-	"l": &Phone{
-		Type:           Liquid,
-		Duration:       0.7,
-		Formants:       [3]float64{550, 1000, 2490},
-		FormantVolumes: [3]float64{0.7, 0.7, 0.7},
-	},
-	"ɹ": &Phone{
-		Type:           Liquid,
-		Duration:       0.7,
-		Formants:       [3]float64{710, 1100, 2540},
-		FormantVolumes: [3]float64{0.5, 0.5, 0.5},
-	},
-	"j": &Phone{
-		Type:           Glide,
-		Duration:       0.2,
-		Formants:       [3]float64{280, 2250, 2890},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"w": &Phone{
-		Type:           Glide,
-		Duration:       0.2,
-		Formants:       [3]float64{310, 870, 2250},
-		FormantVolumes: [3]float64{1, 1, 1},
-	},
-	"ʔ": &Phone{
-		Type:           Stop,
-		Duration:       0.2,
-		Formants:       [3]float64{100, 100, 100},
-		FormantVolumes: [3]float64{0, 0, 0},
-		Unvoiced:       true,
-	},
-}}
+}
