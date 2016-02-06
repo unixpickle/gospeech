ce3c393b849b479b0b49fb17196fb310f3e95219
Alex Nichol <unixpickle@gmail.com>
2016-02-06 14:37:49 -0500

    Created VocalSystem type and rewrote Phone system
    Now, Phones express themselves by messing with the tracks in a VocalSystem. Phones have complete control over which elements of the VocalSystem they utilize, and it is up to each Phone to figure out how to transition from the last phone.
    
    Right now, I have created very basic Phone implementations. They probably will not result in nice-sounding speech, but it's a start. I tried to take some cues from the prototypes, but I did not struggle to make sure that the Phone system will produce everything the same way the prototypes do.

--- phone.go
+++ phone.go
@@ -1,138 +1,256 @@
 package gospeech
 
 import (
-	"math"
-	"math/rand"
+	"time"
 
-	"github.com/unixpickle/wav"
+	"github.com/unixpickle/gospeech/tracks"
 )
 
-// transitionFraction is the fraction of a stop or fricative's
-// time that is spent transitioning between phones.
-const transitionFraction = 0.5
+type Phone interface {
+	// EncodeBeginning encodes the beginning of the phone into the given vocal system.
+	// This should also encode the transition from the previous phone, if applicable.
+	// The lastPhone argument will be nil if this is the first phone in the word.
+	EncodeBeginning(system VocalSystem, lastPhone Phone)
 
-// consonantPull is the amount of influence consonants should have on
-// their neighboring vowels, measured from 0 to 1.
-const consonantPull = 0.2
-
-const maxFormantAmplitude = 0.3
-const maxNoiseAmplitude = 0.5
-
-const aspirationDuration = 0.1
-
-type PhoneType int
-
-const (
-	Stop PhoneType = iota
-	Fricative
-	Nasal
-	Liquid
-	Glide
-	Vowel
-)
-
-type Phone struct {
-	Type     PhoneType
-	Unvoiced bool
-
-	// Duration is the length of the phone in relative units.
-	// The average Duration for a given voice should be about 1.
-	Duration float64
-
-	// For vowels, these fields determine the phone's sound.
-	// For consonants, these determine how vowel-consonant transitions
-	// should take place.
-	Formants       [3]float64
-	FormantVolumes [3]float64
-
-	// These fields describe the noise produced by a fricative consonant.
-	NoiseFrequency float64
-	NoiseSpread    float64
-	NoiseVolume    float64
+	// FormantPull tells the next phone how its formants should be initially modified.
+	// It returns the initial formants for the next phone, given the steady-state formants of said
+	// phone.
+	FormantPull(nextFormant FormantState) FormantState
 }
 
-func (p *Phone) Synthesize(last *Phone, sound wav.Sound, phoneRate float64) {
-	if last == nil {
-		p.synthesizeStatic(sound, phoneRate)
-		return
-	}
-	p.synthesizeTransition(last, sound, phoneRate)
-	p.synthesizeStatic(sound, phoneRate)
+type Vowel struct {
+	Formants FormantState
+	Duration time.Duration
 }
 
-func (p *Phone) synthesizeStatic(sound wav.Sound, phoneRate float64) {
-	sampleCount := int(float64(sound.SampleRate()) * p.Duration / phoneRate)
-	samples := make([]wav.Sample, sampleCount)
-	noise := newNoiseGenerator(p.NoiseFrequency, p.NoiseSpread)
-	for i := 0; i < sampleCount; i++ {
-		secondsElapsed := float64(i) / float64(sound.SampleRate())
-
-		var s wav.Sample
-		for j := 0; j < 3; j++ {
-			var frequency, amplitude float64
-			if p.Type == Nasal {
-				frequency = []float64{250, 2500, 3250}[j]
-				amplitude = []float64{0.3, 0.1, 0.05}[j]
-			} else {
-				frequency = p.Formants[j]
-				amplitude = p.FormantVolumes[j] * maxFormantAmplitude
-			}
-			wavValue := math.Sin(math.Pi * 2 * secondsElapsed * frequency)
-			s += wav.Sample(wavValue * amplitude)
-		}
-		if p.NoiseVolume > 0 {
-			noiseSample := noise.nextSample(secondsElapsed)
-			s += wav.Sample(noiseSample * p.NoiseVolume * maxNoiseAmplitude)
-		}
-		samples[i] = s
+func (v Vowel) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if lastPhone != nil {
+		startFormant := lastPhone.FormantPull(v.Formants)
+		system.AdjustFormants(startFormant, v.Duration/6)
+		system.AdjustFormants(v.Formants, v.Duration/2)
+	} else {
+		startFormants := v.Formants
+		startFormants.Volumes = [3]float64{}
+		system.AdjustFormants(startFormants, v.Duration/6)
+		system.AdjustFormants(v.Formants, v.Duration/2)
 	}
-
-	if p.Type == Stop {
-		asperationSamples := int(float64(sound.SampleRate()) / phoneRate * aspirationDuration)
-		for i := 0; i < asperationSamples; i++ {
-			samples = append(samples, wav.Sample(rand.Float64()*2-1))
-		}
-	}
-
-	sound.SetSamples(append(sound.Samples(), samples...))
+	system.Turbulence().AdjustVolume(0, v.Duration/4)
+	system.ConsonantVoice().AdjustVolume(0, v.Duration/3)
+	system.Liquid().AdjustVolume(0, v.Duration/3)
+	system.FormantsTrack().Continue(v.Duration / 3)
+	system.EvenOut()
 }
 
-func (p *Phone) synthesizeTransition(last *Phone, sound wav.Sound, phoneRate float64) {
-	transitionTime := math.Min(last.Duration, p.Duration) * transitionFraction
-	transitionSamples := int(float64(sound.SampleRate()) * transitionTime / phoneRate)
+func (v Vowel) FormantPull(nextFormant FormantState) FormantState {
+	return v.Formants
+}
 
-	samples := make([]wav.Sample, transitionSamples)
+// A BilabialPlosive represents a "b" or "p" sound.
+type BilabialPlosive struct {
+	Voiced bool
+}
 
-	for i := 0; i < transitionSamples; i++ {
-		fraction := float64(i) / float64(transitionSamples)
-		fractionNew := fraction
-		if last.Type != Vowel && p.Type == Vowel {
-			fractionNew = consonantPull*fraction + fraction*(1-consonantPull)
-		} else if p.Type != Vowel && last.Type == Vowel {
-			fractionNew = consonantPull * fraction
-		}
-
-		var formants [3]float64
-		var formantVolumes [3]float64
-		for j := 0; j < 3; j++ {
-			lastFormant := last.Formants[j]
-			newFormant := p.Formants[j]
-			lastVolume := last.FormantVolumes[j]
-			newVolume := p.FormantVolumes[j]
-			formants[j] = lastFormant*(1-fractionNew) + newFormant*fractionNew
-			formantVolumes[j] = lastVolume*(1-fractionNew) + newVolume*fractionNew
-		}
-
-		var s wav.Sample
-		for j := 0; j < 3; j++ {
-			frequency := formants[j]
-			amplitude := formantVolumes[j] * maxFormantAmplitude
-			secondsElapsed := float64(i) / float64(sound.SampleRate())
-			wavValue := math.Sin(math.Pi * 2 * secondsElapsed * frequency)
-			s += wav.Sample(wavValue * amplitude)
-		}
-		samples[i] = s
+func (b BilabialPlosive) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(b.FormantPull(system.Formants()), time.Millisecond*50)
 	}
+	system.Turbulence().AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0, time.Millisecond*50)
 
-	sound.SetSamples(append(sound.Samples(), samples...))
+	system.Continue(time.Millisecond * 10)
+	if b.Voiced {
+		system.ConsonantVoice().AdjustVolume(0.3, time.Millisecond*50)
+		system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	}
+	turbulence := system.Turbulence()[tracks.TrackID("B")]
+	turbulence.Continue(time.Millisecond * 20)
+	turbulence.AdjustVolume(0.3, time.Millisecond*3)
+	turbulence.Continue(time.Millisecond * 20)
+	turbulence.AdjustVolume(0, time.Millisecond*20)
+	system.EvenOut()
+}
+
+func (b BilabialPlosive) FormantPull(end FormantState) FormantState {
+	end.Volumes = [3]float64{}
+	for i := 0; i < 3; i++ {
+		end.Frequencies[i] *= 0.9
+	}
+	return end
+}
+
+// An AlveolarPlosive represents a "t" or "d" sound.
+type AlveolarPlosive struct {
+	Voiced bool
+
+	// ContinueToNext indicates that the next phone is an "s" or something like that, in which case
+	// this phone needn't terminate its sound.
+	ContinueToNext bool
+}
+
+func (a AlveolarPlosive) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(a.FormantPull(system.Formants()), time.Millisecond*50)
+	}
+	system.Turbulence().AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0, time.Millisecond*50)
+
+	system.Continue(time.Millisecond * 10)
+	if a.Voiced {
+		system.ConsonantVoice().AdjustVolume(0.3, time.Millisecond*50)
+		if !a.ContinueToNext {
+			system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+		}
+	}
+	turbulence := system.Turbulence()[tracks.TrackID("S")]
+	turbulence.Continue(time.Millisecond * 20)
+	turbulence.AdjustVolume(0.3, time.Millisecond*3)
+	turbulence.Continue(time.Millisecond * 20)
+	if !a.ContinueToNext {
+		turbulence.AdjustVolume(0, time.Millisecond*20)
+	}
+	system.EvenOut()
+}
+
+func (a AlveolarPlosive) FormantPull(end FormantState) FormantState {
+	// TODO: figure out some values here. It probably varies per vowel.
+	end.Volumes = [3]float64{}
+	return end
+}
+
+// A VelarPlosive represents a "k" or "g" sound.
+type VelarPlosive struct {
+	Voiced bool
+}
+
+func (v VelarPlosive) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(v.FormantPull(system.Formants()), time.Millisecond*50)
+	}
+	system.Turbulence().AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0, time.Millisecond*50)
+
+	system.Continue(time.Millisecond * 10)
+	if v.Voiced {
+		system.ConsonantVoice().AdjustVolume(0.3, time.Millisecond*50)
+		system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	}
+	turbulence := system.Turbulence()[tracks.TrackID("K")]
+	turbulence.Continue(time.Millisecond * 20)
+	turbulence.AdjustVolume(0.3, time.Millisecond*3)
+	turbulence.Continue(time.Millisecond * 20)
+	turbulence.AdjustVolume(0, time.Millisecond*20)
+	system.EvenOut()
+}
+
+func (v VelarPlosive) FormantPull(end FormantState) FormantState {
+	res := end
+	res.Volumes = [3]float64{}
+	res.Frequencies[0] = end.Frequencies[0]*0.9 + end.Frequencies[1]*0.1
+	res.Frequencies[1] = end.Frequencies[1]*0.9 + end.Frequencies[0]*0.1
+	return res
+}
+
+// A Nasal represents an "n", "m", or "ng" sound.
+type Nasal struct {
+	// Type is either "n", "m", or "ng", and dictates the formant pull technique.
+	Type     string
+	Formants FormantState
+}
+
+func (n Nasal) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(n.FormantPull(system.Formants()), time.Millisecond*50)
+	}
+	system.Turbulence().AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0, time.Millisecond*50)
+
+	system.AdjustFormants(n.Formants, time.Millisecond*100)
+	system.EvenOut()
+	system.Continue(time.Millisecond * 50)
+}
+
+func (n Nasal) FormantPull(end FormantState) FormantState {
+	// TODO: figure out a more accurate pull technique for nasals.
+	var pullFrequencies [3]float64
+	switch n.Type {
+	case "ng":
+		return VelarPlosive{}.FormantPull(end)
+	case "n":
+		pullFrequencies = [3]float64{500, 1500, 2490}
+	case "m":
+		pullFrequencies = [3]float64{550, 1000, 2490}
+	}
+	end.Volumes = [3]float64{}
+	for i := 0; i < 3; i++ {
+		end.Frequencies[i] = end.Frequencies[i]*0.9 + pullFrequencies[i]*0.1
+	}
+	return end
+}
+
+// A Fricative represents any consonant which is characterized by turbulent airflow.
+type Fricative struct {
+	// Type is "F", "TH", "S", "SH", or "H", indicating which kind of fricative this is.
+	Type string
+
+	Voiced bool
+}
+
+func (f Fricative) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(f.FormantPull(system.Formants()), time.Millisecond*50)
+	}
+	system.Turbulence().ExcludeTracks(tracks.TrackID(f.Type)).AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0, time.Millisecond*50)
+
+	turbulence := system.Turbulence()[tracks.TrackID(f.Type)]
+	turbulence.AdjustVolume(0.3, time.Millisecond*100)
+	turbulence.AdjustVolume(0, time.Millisecond*200)
+	system.EvenOut()
+}
+
+func (f Fricative) FormantPull(end FormantState) FormantState {
+	end.Volumes = [3]float64{}
+	return end
+}
+
+// An AlveolarApproximant represents an "r" sound (without trill).
+type AlveolarApproximant struct {
+	Formants FormantState
+}
+
+func (a AlveolarApproximant) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	vowel := Vowel{Formants: a.Formants, Duration: time.Millisecond * 150}
+	vowel.EncodeBeginning(system, lastPhone)
+}
+
+func (a AlveolarApproximant) FormantPull(end FormantState) FormantState {
+	return a.Formants
+}
+
+// An AlveolarLateralApproximant represents an "l" sound.
+type AlveolarLateralApproximant struct{}
+
+func (a AlveolarLateralApproximant) EncodeBeginning(system VocalSystem, lastPhone Phone) {
+	if system.FormantsTrack().Volume() > 0 {
+		system.AdjustFormants(a.FormantPull(system.Formants()), time.Millisecond*50)
+	}
+	system.Turbulence().AdjustVolume(0, time.Millisecond*50)
+	system.ConsonantVoice().AdjustVolume(0, time.Millisecond*50)
+	system.Liquid().AdjustVolume(0.3, time.Millisecond*100)
+	system.EvenOut()
+	system.Continue(time.Millisecond * 100)
+}
+
+func (a AlveolarLateralApproximant) FormantPull(end FormantState) FormantState {
+	end.Volumes = [3]float64{}
+	pullFrequencies := [3]float64{450, 1030, 2380}
+	for i := 0; i < 3; i++ {
+		end.Frequencies[i] = end.Frequencies[i]*0.9 + pullFrequencies[i]*0.1
+	}
+	return end
 }
---
+++ vocal_system.go
@@ -0,0 +1,96 @@
+package gospeech
+
+import (
+	"time"
+
+	"github.com/unixpickle/gospeech/tracks"
+)
+
+// FormantState represents an instantaneous state of three formants.
+type FormantState struct {
+	Frequencies [3]float64
+	Volumes     [3]float64
+}
+
+// A VocalSystem manages speech-like qualities in a TrackSet.
+type VocalSystem struct {
+	tracks.TrackSet
+}
+
+// NewVocalSystem creates a VocalSystem that is currently silent.
+func NewVocalSystem() VocalSystem {
+	return VocalSystem{tracks.TrackSet{
+		"Formants": tracks.TrackSet{
+			"F1": tracks.NewToneTrack(400, 0, 0),
+			"F2": tracks.NewToneTrack(1000, 0, 0),
+			"F3": tracks.NewToneTrack(2000, 0, 0),
+		},
+		"Turbulence": tracks.TrackSet{
+			"S":  tracks.NewToneTrack(5000, 0, 1000),
+			"SH": tracks.NewToneTrack(4700, 0, 1000),
+			"TH": tracks.NewToneTrack(3500, 0, 1000),
+			"B":  tracks.NewToneTrack(4250, 0, 1000),
+			"F":  tracks.NewToneTrack(4250, 0, 1000),
+			"K":  tracks.NewToneTrack(4250, 0, 1000),
+			"H": tracks.TrackSet{
+				"F1": tracks.NewToneTrack(280, 0, 280),
+				"F2": tracks.NewToneTrack(2250, 0, 500),
+				"F3": tracks.NewToneTrack(2890, 0, 500),
+			},
+		},
+		"ConsonantVoice": tracks.TrackSet{
+			"Humm1": tracks.NewToneTrack(200, 0, 0),
+			"Humm2": tracks.NewToneTrack(300, 0, 0),
+			"Humm3": tracks.NewToneTrack(400, 0, 0),
+		},
+		"Liquid": tracks.NewToneTrack(500, 0, 0),
+	}}
+}
+
+// FormantsTrack returns the track corresponding to the formants as a whole.
+func (v VocalSystem) FormantsTrack() tracks.TrackSet {
+	return v.TrackSet[tracks.TrackID("Formants")].(tracks.TrackSet)
+}
+
+// Formants returns the current formant state.
+func (v VocalSystem) Formants() FormantState {
+	freqs := map[string]float64{}
+	volumes := map[string]float64{}
+	for name, track := range v.FormantsTrack() {
+		tone := track.(*tracks.ToneTrack)
+		freqs[string(name)] = tone.Frequency()
+		volumes[string(name)] = tone.Volume()
+	}
+	return FormantState{
+		Frequencies: [3]float64{freqs["F1"], freqs["F2"], freqs["F3"]},
+		Volumes:     [3]float64{volumes["F1"], volumes["F2"], volumes["F3"]},
+	}
+}
+
+// AdjustFormants adjusts the formant state over a period of time.
+func (v VocalSystem) AdjustFormants(state FormantState, d time.Duration) {
+	freqs := map[string]float64{"F1": state.Frequencies[0], "F2": state.Frequencies[1],
+		"F3": state.Frequencies[2]}
+	volumes := map[string]float64{"F1": state.Volumes[0], "F2": state.Volumes[1],
+		"F3": state.Volumes[2]}
+	for name, track := range v.FormantsTrack() {
+		n := string(name)
+		track.(*tracks.ToneTrack).AdjustAll(freqs[n], volumes[n], 0, d)
+	}
+}
+
+// Turbulence returns the set of track that correspond to different kinds of turbulent airflow,
+// such as the hissing sound in "s" or the brief expulsion of air in "t".
+func (v VocalSystem) Turbulence() tracks.TrackSet {
+	return v.TrackSet[tracks.TrackID("Turbulence")].(tracks.TrackSet)
+}
+
+// ConsonantVoice returns the track that corresponds to the humming heard in voiced consonants.
+func (v VocalSystem) ConsonantVoice() tracks.Track {
+	return v.TrackSet[tracks.TrackID("ConsonantVoice")]
+}
+
+// Liquid returns the track that corresponds to the "L" sound.
+func (v VocalSystem) Liquid() tracks.Track {
+	return v.TrackSet[tracks.TrackID("ConsonantVoice")]
+}
